fce4aad0457bea6f3003fe22643a88323d922dbb
peter <peter.k.nets@gmail.com>
2015-10-04 21:28:19 +0300

    Use NG-SRC

--- js/main.js
+++ js/main.js
@@ -13,6 +13,16 @@
             name: 'Dodecahedron',
             price: 202.5,
             description: '...',
+            images: [
+                {
+                    full: 'img/Chuck.jpg',
+                    thumb: 'img/Chuck_thumb.jpg'
+                },
+                {
+                    full: 'img/Thug.jpg',
+                    thumb: 'img/Thug_thumb.jpg'
+                }
+            ],
             canPurchase: true,
             soldOut: false
         },
@@ -20,6 +30,16 @@
             name: 'Bibicahedron',
             price: 99.2,
             description: '...',
+            images: [
+                {
+                    full: 'img/Gunman.jpg',
+                    thumb: 'img/Gunman_thumb.jpg'
+                },
+                {
+                    full: 'img/Gunwoman.jpg',
+                    thumb: 'img/Gunwoman_thumb.jpg'
+                }
+            ],
             canPurchase: true,
             soldOut: false
         }
