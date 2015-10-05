5729bef786766d10291a4776b6d3eafa7adbc4a6
peter <peter.k.nets@gmail.com>
2015-10-05 09:47:02 +0300

    Add submit function to live form

--- js/main.js
+++ js/main.js
@@ -17,6 +17,15 @@
             return this.tab === checkTab;
         };
     });
+
+    app.controller('ReviewController', function(){
+        this.review = {};
+        this.addReview = function(product){
+            product.reviews.push(this.review);
+            this.review = {};
+        }
+    });
+
     var gems = [
         {
             name: 'Dodecahedron',
