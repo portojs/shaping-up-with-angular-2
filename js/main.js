953fcb0c6212e6a024391da5ad51fb2ad3e2998c
peter <peter.k.nets@gmail.com>
2015-10-05 10:18:25 +0300

    Add a directive PRODUCT-PANELS that includes a Controller

--- js/main.js
+++ js/main.js
@@ -8,16 +8,6 @@
         this.products = gems;
     });
 
-    app.controller('PanelController', function(){
-        this.tab = 1;
-        this.selectTab = function(setTab){
-            this.tab = setTab;
-        };
-        this.isSelected = function(checkTab){
-            return this.tab === checkTab;
-        };
-    });
-
     app.controller('ReviewController', function(){
         this.review = {};
         this.addReview = function(product){
@@ -30,6 +20,23 @@
         return {
             restrict: 'E',
             templateUrl: 'product-title.html'
+        };
+    });
+
+    app.directive('productPanels', function(){
+        return {
+            restrict: 'E',
+            templateUrl: 'product-panels.html',
+            controller: function(){
+                this.tab = 1;
+                this.selectTab = function(setTab){
+                    this.tab = setTab;
+                };
+                this.isSelected = function(checkTab){
+                    return this.tab === checkTab;
+                };
+            },
+            controllerAs: 'panels'
         };
     });
 
