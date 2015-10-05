2d21d1409317584a372a3acac664558948b4a66e
peter <peter.k.nets@gmail.com>
2015-10-05 11:36:45 +0300

    Add dependency injection $HTTP

--- js/main.js
+++ js/main.js
@@ -4,9 +4,13 @@
 (function(){
     var app = angular.module('store', ['store-products']);
 
-    app.controller('StoreController', function(){
-        this.products = gems;
-    });
+    app.controller('StoreController', [ '$http', function($http){
+        var store = this;
+        store.products = [];
+        $http.get('json/products.json').success(function(data){
+            store.products = data;
+        });
+    }]);
 
     app.controller('ReviewController', function(){
         this.review = {};
@@ -16,64 +20,4 @@
         }
     });
 
-    var gems = [
-        {
-            name: 'Dodecahedron',
-            price: 202.5,
-            description: '...',
-            images: [
-                {
-                    full: 'img/Chuck.jpg',
-                    thumb: 'img/Chuck_thumb.jpg'
-                },
-                {
-                    full: 'img/Thug.jpg',
-                    thumb: 'img/Thug_thumb.jpg'
-                }
-            ],
-            reviews: [
-                {
-                    stars: 4,
-                    body: "...",
-                    author: "Peter"
-                },
-                {
-                    stars: 3,
-                    body: "...",
-                    author: "Bobba"
-                }
-            ],
-            canPurchase: true,
-            soldOut: false
-        },
-        {
-            name: 'Bibicahedron',
-            price: 99.2,
-            description: '...',
-            images: [
-                {
-                    full: 'img/Gunman.jpg',
-                    thumb: 'img/Gunman_thumb.jpg'
-                },
-                {
-                    full: 'img/Gunwoman.jpg',
-                    thumb: 'img/Gunwoman_thumb.jpg'
-                }
-            ],
-            reviews: [
-                {
-                    stars: 5,
-                    body: "...",
-                    author: "Peter"
-                },
-                {
-                    stars: 2,
-                    body: "...",
-                    author: "Bobba"
-                }
-            ],
-            canPurchase: true,
-            soldOut: false
-        }
-    ];
 })();
